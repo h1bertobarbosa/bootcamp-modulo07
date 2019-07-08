d1469cfd39d13e9f6b0affd881c4366c2bd99487
humberto <humberto.obarbosa@gmail.com>
2019-07-08 19:24:47 -0300

    estilizacao da home

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,7 +1,93 @@
 import React from 'react';
-
-// import { Container } from './styles';
+import { MdAddShoppingCart } from 'react-icons/md';
+import { ProductList } from './styles';
 
 export default function Home() {
-  return <div />;
+  return (
+    <ProductList>
+      <li>
+        <img
+          src="https://static.netshoes.com.br/produtos/tenis-kappa-impact-masculino/04/D24-1738-304/D24-1738-304_detalhe2.jpg"
+          alt="Tenis"
+        />
+        <strong>Tenis Massa</strong>
+        <button type="button">
+          <div>
+            <MdAddShoppingCart size={16} color="#fff" /> 3
+          </div>
+          <span>Adicionar ao carrinho</span>
+        </button>
+      </li>
+
+      <li>
+        <img
+          src="https://static.netshoes.com.br/produtos/tenis-kappa-impact-masculino/04/D24-1738-304/D24-1738-304_detalhe2.jpg"
+          alt="Tenis"
+        />
+        <strong>Tenis Massa</strong>
+        <button type="button">
+          <div>
+            <MdAddShoppingCart size={16} color="#fff" /> 3
+          </div>
+          <span>Adicionar ao carrinho</span>
+        </button>
+      </li>
+
+      <li>
+        <img
+          src="https://static.netshoes.com.br/produtos/tenis-kappa-impact-masculino/04/D24-1738-304/D24-1738-304_detalhe2.jpg"
+          alt="Tenis"
+        />
+        <strong>Tenis Massa</strong>
+        <button type="button">
+          <div>
+            <MdAddShoppingCart size={16} color="#fff" /> 3
+          </div>
+          <span>Adicionar ao carrinho</span>
+        </button>
+      </li>
+
+      <li>
+        <img
+          src="https://static.netshoes.com.br/produtos/tenis-kappa-impact-masculino/04/D24-1738-304/D24-1738-304_detalhe2.jpg"
+          alt="Tenis"
+        />
+        <strong>Tenis Massa</strong>
+        <button type="button">
+          <div>
+            <MdAddShoppingCart size={16} color="#fff" /> 3
+          </div>
+          <span>Adicionar ao carrinho</span>
+        </button>
+      </li>
+
+      <li>
+        <img
+          src="https://static.netshoes.com.br/produtos/tenis-kappa-impact-masculino/04/D24-1738-304/D24-1738-304_detalhe2.jpg"
+          alt="Tenis"
+        />
+        <strong>Tenis Massa</strong>
+        <button type="button">
+          <div>
+            <MdAddShoppingCart size={16} color="#fff" /> 3
+          </div>
+          <span>Adicionar ao carrinho</span>
+        </button>
+      </li>
+
+      <li>
+        <img
+          src="https://static.netshoes.com.br/produtos/tenis-kappa-impact-masculino/04/D24-1738-304/D24-1738-304_detalhe2.jpg"
+          alt="Tenis"
+        />
+        <strong>Tenis Massa</strong>
+        <button type="button">
+          <div>
+            <MdAddShoppingCart size={16} color="#fff" /> 3
+          </div>
+          <span>Adicionar ao carrinho</span>
+        </button>
+      </li>
+    </ProductList>
+  );
 }
